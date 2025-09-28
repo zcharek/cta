fae8fd35d852511df0f35636b658a0ac3dd0cd93
Central test agency <centraltestagency@gmail.com>
2025-09-28 14:46:23 +0200

    Fix routing - Use redirect parameter instead of hash routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
-import { Switch, Route, Router } from "wouter";
-import { useHashLocation } from "wouter/use-hash-location";
+import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -44,8 +43,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <Router hook={useHashLocation}>
-          <Switch>
+        <Switch>
           <Route path="/" component={Home} />
           <Route path="/services" component={Services} />
           <Route path="/faq" component={FAQ} />
@@ -80,8 +78,7 @@ function App() {
           <Route path="/services/restassured" component={RestAssured} />
           
           <Route component={NotFound} />
-          </Switch>
-        </Router>
+        </Switch>
         <Toaster />
       </TooltipProvider>
     </QueryClientProvider>
