c895ac5d38ac65fa83f0bb5365b8b43cc08e8f2d
Central test agency <centraltestagency@gmail.com>
2025-09-28 15:05:41 +0200

    Fix routing - Use hash router and direct redirects

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -43,7 +44,8 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <Switch>
+        <Router hook={useHashLocation}>
+          <Switch>
           <Route path="/" component={Home} />
           <Route path="/services" component={Services} />
           <Route path="/faq" component={FAQ} />
@@ -78,7 +80,8 @@ function App() {
           <Route path="/services/restassured" component={RestAssured} />
           
           <Route component={NotFound} />
-        </Switch>
+          </Switch>
+        </Router>
         <Toaster />
       </TooltipProvider>
     </QueryClientProvider>
